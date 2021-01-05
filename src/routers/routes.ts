b893292a583761938bcbccb16ac1cda8e1314f71
Hoang Ho <hitle69@gmail.com>
2021-01-06 00:46:52 +0700

    add register screen

--- src/routers/routes.ts
+++ src/routers/routes.ts
@@ -4,12 +4,17 @@ import { RouteName } from './routeName';
 export type RootStackParams = {
   [RouteName.INTRO]: undefined;
   [RouteName.LOGIN]: undefined;
+  [RouteName.REGISTER]: undefined;
   [RouteName.HOME]: { userId: string };
 };
 export type RootStackParamType = keyof RootStackParams;
 
 export const publicRoutes: RouteProps[] = [
 
+  {
+    name: RouteName.REGISTER,
+    component: require('src/screens/register').default,
+  },
   {
     name: RouteName.INTRO,
     component: require('src/screens/intro').default,
