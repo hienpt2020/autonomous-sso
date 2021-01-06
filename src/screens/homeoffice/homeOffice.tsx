768daa9574d2fd9403b2083c9a40655caadc3289
Hoang Ho <hoang.q@autonomous.nyc>
2021-01-06 15:51:03 +0700

    finish layout home office

---
+++ src/screens/homeoffice/homeOffice.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react'
+import { Image, SectionList, Text, View, Dimensions } from 'react-native';
+import { useState, useEffect } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Header } from 'src/components/header'
+import { useTranslation } from 'react-i18next';
+import { Props, SectionData, Presenter } from './types';
+import { PresenterImpl } from './presenter';
+import { styles } from './styles';
+import { RouteName } from 'src/routers/routeName';
+import { CardItem, CardData } from './card';
+
+const Office = (props: Props) => {
+   
+    const initialData: SectionData[] = []
+    const { t } = useTranslation()
+    const [sectionData, setSectionData] = useState(initialData)
+    const presenter : Presenter = new PresenterImpl()
+
+    useEffect(() => {
+        let floorsResult = presenter.fetchFloor()
+        setSectionData(floorsResult)
+    }, [])
+    const renderItem = (data: CardData) => {
+        return ( <CardItem  cardData={data}/> )
+    }
+    const renderHeader = (title: string) => {
+        return (<Text style={styles.header}>{title}</Text>)
+    }
+    const getItemLayout = (data: any, index:any) => {
+        return ( {length: 180, offset: 180 * index, index})
+    }
+    return (
+        <SafeAreaView style={styles.container}>
+            <Header title={t('office.title')} />
+            <SectionList
+                sections={sectionData}
+                keyExtractor={(item, index) => item.id + index}
+                renderItem={({ item }) => renderItem(item)}
+                getItemLayout={(data, index) => getItemLayout(data, index)}
+                renderSectionHeader={({ section: { title } }) => renderHeader(title)}
+            />
+        </SafeAreaView>
+    )
+
+};
+
+export default Office;
--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,5 +1,6 @@
 export enum AppColor {
   PRIMARY = '#000000',
   WHITE = '#ffffff',
+  LIGHT = '#f4f4f4',
   ALPHA_40 = 'rgba(0, 0, 0, 0.4)',
 }
