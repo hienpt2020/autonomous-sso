b9442b9205fbe2956252ee34af7b70176f0f0a08
Hoang Ho <hoang.q@autonomous.nyc>
2021-01-06 09:02:47 +0700

    fix issue that change app version when run make file

--- tools/run.sh
+++ tools/run.sh
@@ -28,12 +28,15 @@ fi
 
 PLATFORM=$1
 ENV=$2
+BUILDTYPE=$3
 
 # Update new APP_BUILD_NUMBER value
 export $(xargs < .env.${ENV})
-
-NEW_VALUE=`cat .env.${ENV} | sed "s/APP_BUILD_NUMBER=${APP_BUILD_NUMBER}/APP_BUILD_NUMBER=$((${APP_BUILD_NUMBER}+1))/g"`
-echo "${NEW_VALUE}" > .env.${ENV}
+if [[ ! -z "$BUILDTYPE"  ]]; then
+  echo "$BUILDTYPE & update new APP_BUILD_NUMBER"
+  NEW_VALUE=`cat .env.${ENV} | sed "s/APP_BUILD_NUMBER=${APP_BUILD_NUMBER}/APP_BUILD_NUMBER=$((${APP_BUILD_NUMBER}+1))/g"`
+  echo "${NEW_VALUE}" > .env.${ENV}
+fi
 
 if [[ ${PLATFORM} = "android" ]]; then
   ./tools/run-android.sh $ENV $3
