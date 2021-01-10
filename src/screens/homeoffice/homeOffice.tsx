bd80a11abb2d4c56d3fa69dd49f949958fa50cba
hienpham2102 <hienpham2102@gmail.com>
2021-01-10 18:20:30 +0700

    handle get list workplace done

--- src/screens/homeoffice/homeOffice.tsx
+++ src/screens/homeoffice/homeOffice.tsx
@@ -1,8 +1,8 @@
-import * as React from 'react'
-import { Image, SectionList, Text, View, Dimensions } from 'react-native';
-import { useState, useEffect } from 'react'
+import * as React from 'react';
+import { Image, SectionList, Text, View, Dimensions, FlatList } from 'react-native';
+import { useState, useEffect } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Header } from 'src/components/header'
+import { Header } from 'src/components/header';
 import { useTranslation } from 'react-i18next';
 import { Props, SectionData, Presenter } from './types';
 import { PresenterImpl } from './presenter';
@@ -10,44 +10,53 @@ import { styles } from './styles';
 import { RouteName } from 'src/routers/routeName';
 import { CardItem, CardData } from './card';
 import { Card } from 'react-native-elements';
+import { useDispatch, useSelector } from 'react-redux';
+import { getWorkplaceLayoutStartAction } from '../../redux/workplace/workplaceAction';
 
 const Office = (props: Props) => {
+  // const initialData: SectionData[] = [];
+  const { t } = useTranslation();
+  // const [sectionData, setSectionData] = useState(initialData);
+  const dispatch = useDispatch();
+  const presenter: Presenter = new PresenterImpl();
+  const { items } = useSelector((state) => state.workplaceReducer);
 
-    const initialData: SectionData[] = []
-    const { t } = useTranslation()
-    const [sectionData, setSectionData] = useState(initialData)
-    const presenter: Presenter = new PresenterImpl()
-
-    useEffect(() => {
-        let floorsResult = presenter.fetchFloor()
-        setSectionData(floorsResult)
-    }, [])
-    const renderItem = (data: CardData) => {
-        return (<CardItem cardData={data} onPress={() => onItemSelected(data)} />)
-    }
-    const renderHeader = (title: string) => {
-        return (<Text style={styles.header}>{title}</Text>)
-    }
-    const getItemLayout = (data: any, index: any) => {
-        return ({ length: 180, offset: 180 * index, index })
-    }
-    return (
-        <SafeAreaView style={styles.container}>
-            <Header title={t('office.title')} />
-            <SectionList
-                sections={sectionData}
-                stickySectionHeadersEnabled={false}
-                keyExtractor={(item, index) => item.id + index}
-                renderItem={({ item }) => renderItem(item)}
-                getItemLayout={(data, index) => getItemLayout(data, index)}
-                renderSectionHeader={({ section: { title } }) => renderHeader(title)}
-            />
-        </SafeAreaView>
-    )
-    function onItemSelected(data: CardData) {
-        props.navigation.navigate(RouteName.MAP, { floorId: data.id, floorName: data.name })
-    }
-
+  useEffect(() => {
+    dispatch(getWorkplaceLayoutStartAction());
+    // let floorsResult = presenter.fetchFloor();
+    // setSectionData(floorsResult);
+  }, []);
+  const renderItem = (data: CardData) => {
+    return <CardItem cardData={data} onPress={() => onItemSelected(data)} />;
+  };
+  // const renderHeader = (title: string) => {
+  //   return <Text style={styles.header}>{title}</Text>;
+  // };
+  // const getItemLayout = (data: any, index: any) => {
+  //   return { length: 180, offset: 180 * index, index };
+  // };
+  // console.log('@nsandnasdnsadnsda:', items);
+  return (
+    <SafeAreaView style={styles.container}>
+      <Header title={t('office.title')} />
+      {/*<SectionList*/}
+      {/*  sections={sectionData}*/}
+      {/*  stickySectionHeadersEnabled={false}*/}
+      {/*  keyExtractor={(item, index) => item.id + index}*/}
+      {/*  renderItem={({ item }) => renderItem(item)}*/}
+      {/*  getItemLayout={(data, index) => getItemLayout(data, index)}*/}
+      {/*  renderSectionHeader={({ section: { title } }) => renderHeader(title)}*/}
+      {/*/>*/}
+      <FlatList
+        data={presenter.formatOffice(items)}
+        renderItem={({ item }) => renderItem(item)}
+        keyExtractor={(item) => item.id + ''}
+      />
+    </SafeAreaView>
+  );
+  function onItemSelected(data: CardData) {
+    props.navigation.navigate(RouteName.MAP, { floorId: data.id, floorName: data.name });
+  }
 };
 
 export default Office;
