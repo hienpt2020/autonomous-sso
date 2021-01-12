1af79e70c5d9e06e15c094e6c06e45b81df3a872
Hoang Ho <hoang.q@autonomous.nyc>
2021-01-12 09:13:40 +0700

    integrate profile screen

--- src/screens/homeprofile/homeProfile.tsx
+++ src/screens/homeprofile/homeProfile.tsx
@@ -1,30 +1,45 @@
-import * as React from 'react'
-import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { Image } from 'react-native'
+import _ from 'lodash';
+import * as React from 'react';
+import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { Image, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-
-import { createRequestLogoutAction } from 'src/redux/user'
-import { Props } from './types';
-import { styles } from './styles';
-import { Link } from 'src/components/link';
+import { useDispatch, useSelector } from 'react-redux';
+import Icon from 'src/assets/images/empty.svg';
 import { PrimaryButton, SecondaryButton } from 'src/components/button';
+import { Link } from 'src/components/link';
+import { RootState } from 'src/redux/types';
+import { createRequestLogoutAction } from 'src/redux/user';
 import { RouteName } from 'src/routers/routeName';
+import { styles } from './styles';
+import { Props } from './types';
+
 
 const Profile = (props: Props) => {
     const { t } = useTranslation()
+    const userReducer = useSelector((state: RootState) => state.userReducer)
     const dispatch = useDispatch()
+    const [email, setEmail] = useState("")
+    const [avatar, setAvatar] = useState("")
+    const [workspace, setWorkSpace] = useState("")
 
     useEffect(() => {
-    }, [])
+        setEmail(_.get(userReducer, "email", ""))
+        setAvatar(_.get(userReducer, "userAvatar", ""))
+        //TODO integrate workspace 
+        setWorkSpace(_.get(userReducer, "currentWorkspace", "Autonomous"))
+    }, [userReducer.email, userReducer.userAvatar])
 
-    const email = "hoang.q@autonomous.nyc"
-    const workspace = "Autonomous"
-    const avatar = "https://source.unsplash.com/wgivdx9dBdQ/360x180"
+
     return (
         <SafeAreaView style={styles.container}>
-            <Image source={{ uri: avatar }} style={styles.avatar} />
+            <View style={styles.avatarContainer}>
+                {avatar ?
+                    <Image source={{ uri: avatar }} style={styles.avatar} />
+                    : <Icon width={128} height={128} style={styles.avatar} />
+                }
+            </View>
+
             <Link title={email} />
             <PrimaryButton wrapperContainer={styles.containerButton}
                 onPress={() => props.navigation.navigate(RouteName.SWITCH_WORKSPACE)}
