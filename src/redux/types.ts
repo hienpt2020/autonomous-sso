9a1479f2a06f37b16b5c44dc686b7df454d7c058
Hoang Ho <hoang.q@autonomous.nyc>
2021-01-14 09:54:26 +0700

    show workspace information

--- src/redux/types.ts
+++ src/redux/types.ts
@@ -1,6 +1,7 @@
 import { BookingState } from './booking/bookingType';
 import { RequestState } from './request/requestType';
 import { UserState } from './user/userType';
+import { WorkspaceState } from './workspace/workspaceType';
 export type { UserState };
 
 export interface BaseState {
@@ -12,4 +13,5 @@ export interface RootState {
   requestReducer: RequestState;
   userReducer: UserState;
   booking: BookingState;
+  workspaceReducer: WorkspaceState;
 }
