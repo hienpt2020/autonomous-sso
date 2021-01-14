58145345a6e2ce6f7d80b09d17644b04752bbe2f
Hoang Ho <hoang.q@autonomous.nyc>
2021-01-14 09:02:39 +0700

    add fetch workspace when fetching user dât

---
+++ src/redux/user/request/fetchUserData.ts
@@ -0,0 +1,36 @@
+import i18next from 'i18next';
+import _ from 'lodash';
+import { all, call, put } from 'redux-saga/effects';
+import { Preference } from 'src/common/preference';
+import { createRequestErrorMessageAction } from 'src/redux/request/requestAction';
+import { createActionSetWorkSpace } from 'src/redux/workspace/workspaceAction';
+import { createSetUserProfileAction } from '../userAction';
+import { retrieveUserProfile, retrieveUserWorkspace } from './apiUser';
+
+function* fetchUserProfile() {
+    // Get user profile
+    const { userProfile, error } = yield call(retrieveUserProfile);
+    if (userProfile) {
+        //update store
+        userProfile.accessToken = yield call(Preference.retrieveAccessToken);
+        userProfile.isValidToken = true;
+        yield put(createSetUserProfileAction(userProfile));
+    } else {
+        const message = _.get(error, 'errorMessage', i18next.t('common.error'));
+        yield put(createRequestErrorMessageAction(message));
+    }
+}
+function* fetchUserWorkspace() {
+    // Get user profile
+    const { userWorkspace, error } = yield call(retrieveUserWorkspace);
+    if (userWorkspace) {
+        //update store
+        yield put(createActionSetWorkSpace(userWorkspace));
+    } else {
+        const message = _.get(error, 'errorMessage', i18next.t('common.error'));
+        yield put(createRequestErrorMessageAction(message));
+    }
+}
+export function* fetchUserData() {
+    yield all([call(fetchUserProfile), call(fetchUserWorkspace)]);
+}
--- src/redux/user/request/requestLoginAction.ts
+++ src/redux/user/request/requestLoginAction.ts
@@ -1,46 +1,15 @@
 import i18next from 'i18next';
 import _ from 'lodash';
 import { call, put } from 'redux-saga/effects';
-import { Preference } from 'src/common/preference';
 import {
     createRequestEndAction,
     createRequestErrorMessageAction,
     createRequestStartAction
 } from 'src/redux/request/requestAction';
-import { User } from 'src/models';
-import { createActionSetWorkSpace } from 'src/redux/workspace/workspaceAction';
 import { NetworkingConfig } from 'src/services/networking';
-import { createSetUserProfileAction } from '../userAction';
-import { requestLogin, retrieveUserProfile, retrieveUserWorkspace } from './apiUser';
+import { requestLogin } from './apiUser';
+import { fetchUserData } from './fetchUserData';
 
-function* fetchUserProfile() {
-    // Get user profile
-    const { userProfile, error } = yield call(retrieveUserProfile);
-    if (userProfile) {
-        //update store
-        userProfile.accessToken = yield call(Preference.retrieveAccessToken);
-        userProfile.isValidToken = true
-        yield put(createSetUserProfileAction(userProfile));
-    } else {
-        const message = _.get(error, 'errorMessage', i18next.t('common.error'));
-        yield put(createRequestErrorMessageAction(message));
-    }
-}
-function* fetchUserWorkspace() {
-    // Get user profile
-    const { userWorkspace, error } = yield call(retrieveUserWorkspace);
-    if (userWorkspace) {
-        //update store
-        yield put(createActionSetWorkSpace(userWorkspace));
-    } else {
-        const message = _.get(error, 'errorMessage', i18next.t('common.error'));
-        yield put(createRequestErrorMessageAction(message));
-    }
-}
-function* fetchUserData() {
-    yield call(fetchUserProfile);
-    yield call(fetchUserWorkspace);
-}
 export function* requestLoginAction(action: any) {
     yield put(createRequestStartAction());
     const { response, error } = yield call(requestLogin, action.payload.email, action.payload.password);
