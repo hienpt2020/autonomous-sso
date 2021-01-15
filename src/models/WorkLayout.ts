23feb2e0f4f74826a7b97e5d1164b017cb9e2345
Loc Lee <dailoc@autonomous.nyc>
2021-01-15 13:01:41 +0700

    Add some data parser

--- src/models/WorkLayout.ts
+++ src/models/WorkLayout.ts
@@ -5,11 +5,11 @@ export default class WorkLayout {
   image: string;
   policy: string;
 
-  constructor(id: number, name: string, address: string, image: string, policy: string) {
-    this.id = id;
-    this.name = name;
-    this.address = address;
-    this.image = image;
-    this.policy = policy;
+  constructor() {
+    this.id = 0;
+    this.name = '';
+    this.address = '';
+    this.image = '';
+    this.policy = '';
   }
 }
