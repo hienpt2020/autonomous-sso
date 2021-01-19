fc3da296df46a3772c69478d58904b4a1231c98f
Ban <leloc@waves8.com>
2021-01-19 22:02:21 +0700

    Update Bottom Tab Bar

--- src/screens/home/styles.tsx
+++ src/screens/home/styles.tsx
@@ -1,13 +1,16 @@
 import { StyleSheet } from 'react-native';
-import { AppColor } from 'src/styles';
+import { AppColor, AppFontSize } from 'src/styles';
 
 export const styles = StyleSheet.create({
-  iconContainer: {
-    width: 70,
-    height: 40,
-    backgroundColor: AppColor.PRIMARY,
-    borderRadius: 20,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
+    label: {
+        color: AppColor.GREY_1,
+        fontSize: AppFontSize.SIZE_12,
+        marginTop: -5,
+    },
+    labelActive: {
+        color: AppColor.BLUE_1,
+        fontSize: AppFontSize.SIZE_12,
+        fontWeight: '500',
+        marginTop: -5,
+    },
 });
