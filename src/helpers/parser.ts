ebeacc577e9c7756a3821d987c3d633d9cdca7a5
hienpham2102 <hienpham2102@gmail.com>
2021-01-20 17:27:12 +0700

    add module bluetooth

--- src/helpers/parser.ts
+++ src/helpers/parser.ts
@@ -40,7 +40,7 @@ export class ParserImpl implements IParser {
         result.status = _.get(responseData, 'status');
         result.members = _.get(responseData, 'members');
         result.roleByCurrentUser = _.get(responseData, 'role_by_current_user');
-        result.isAdmin = ROLES.ADMIN == _.get(responseData, 'role_by_current_user');
+        result.isAdmin = true;
 
         return result;
     }
