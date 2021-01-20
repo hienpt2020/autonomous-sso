089d8e5344b3b7b69c4f7d58df0ae351c1ee5399
hienpham2102 <hienpham2102@gmail.com>
2021-01-20 13:55:30 +0700

    convert class to func hook and add field code

--- src/models/Booking.ts
+++ src/models/Booking.ts
@@ -1,9 +1,10 @@
 export default class Booking {
-  from: Date;
-  to: Date;
-
-  constructor(from: Date, to: Date) {
-    this.from = from;
-    this.to = to;
-  }
+    from: Date;
+    to: Date;
+    code: string;
+    constructor(from: Date, to: Date, code: string) {
+        this.from = from;
+        this.to = to;
+        this.code = code;
+    }
 }
--- src/screens/configuration1/configuration.tsx
+++ src/screens/configuration1/configuration.tsx
@@ -1,16 +1,5 @@
 import * as React from 'react';
-import {
-    View,
-    Text,
-    StatusBar,
-    FlatList,
-    ScrollView,
-    NativeModules,
-    NativeEventEmitter,
-    AppState,
-    Platform,
-} from 'react-native';
-import { IState } from './types';
+import { View, Text, StatusBar, FlatList, ScrollView, NativeModules, NativeEventEmitter, AppState } from 'react-native';
 import { styles } from './styles';
 import { PrimaryButton } from 'src/components/button';
 import { YellowBox } from 'react-native';
@@ -19,13 +8,14 @@ import CardData from './CardData';
 import BleManager from 'react-native-ble-manager';
 import { navigate } from '../../routers/rootNavigation';
 import { RouteName } from '../../routers/routeName';
-import { connect } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { createRequestEndAction, createRequestErrorMessageAction } from '../../redux/request';
 import { BackHeader } from '../../components/header';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { ConfigStep1Actions, IConfigStep1Actions } from './actions/configurationAction';
 import { Parser } from '../../helpers/parser';
 import i18next from 'i18next';
+import { useEffect, useState } from 'react';
 //JUST disable this warning
 YellowBox.ignoreWarnings([
     'VirtualizedLists should never be nested', // TODO: Remove when fixed
@@ -33,149 +23,142 @@ YellowBox.ignoreWarnings([
 
 const BleManagerModule = NativeModules.BleManager;
 const bleManagerEmitter = new NativeEventEmitter(BleManagerModule);
-const IS_IOS = Platform.OS === 'ios';
 
-class ConfigurationStep1 extends React.Component {
-    servicesInfo: any;
-    handlerDiscoverEmitter: any;
-    handlerStopScanEmitter: any;
-    handlerDisconnectEmitter: any;
-    handlerBleNotificationEmitter: any;
-    handlerConnectedPerEmitter: any;
-    connectedPeripheralId: string = '';
-    configAction: IConfigStep1Actions;
+const ConfigurationStep1 = (props: any) => {
+    const [appState, setAppState] = useState<any>('');
+    const [list, setList] = useState<any[]>([]);
+    const imageHeight = 221;
+    const dispatch = useDispatch();
+    const configAction: IConfigStep1Actions = new ConfigStep1Actions(dispatch);
+    let peripherals = new Map();
+    let handlerDiscoverEmitter: any;
+    let handlerStopScanEmitter: any;
+    let handlerDisconnectEmitter: any;
+    let handlerBleNotificationEmitter: any;
+    let handlerConnectedPerEmitter: any;
+    let connectedPeripheralId: string = '';
 
-    state: IState;
-    constructor(props: any) {
-        super(props);
-        this.state = {
-            scanning: false,
-            peripherals: new Map(),
-            appState: '',
-            peripheral: '',
-            selected: '',
+    useEffect(() => {
+        init();
+        return () => {
+            clearEvents();
         };
-        this.configAction = new ConfigStep1Actions(props.dispatch);
-    }
+    }, []);
 
-    handleAppStateChange = (nextAppState: any) => {
-        if (this.state.appState.match(/inactive|background/) && nextAppState === 'active') {
+    const init = async () => {
+        AppState.addEventListener('change', handleAppStateChange);
+        await BleManager.start({ showAlert: false });
+        handlerDiscoverEmitter = bleManagerEmitter.addListener(
+            'BleManagerDiscoverPeripheral',
+            handleDiscoverPeripheral,
+        );
+        handlerStopScanEmitter = bleManagerEmitter.addListener('BleManagerStopScan', handleStopScan);
+        handlerDisconnectEmitter = bleManagerEmitter.addListener(
+            'BleManagerDisconnectPeripheral',
+            handleDisconnectedPeripheral,
+        );
+        handlerBleNotificationEmitter = bleManagerEmitter.addListener(
+            'BleManagerDidUpdateValueForCharacteristic',
+            readBleNotification,
+        );
+        handlerConnectedPerEmitter = bleManagerEmitter.addListener(
+            'BleManagerConnectPeripheral',
+            handleConnectedPeripheral,
+        );
+        await configAction.getMqttInfo();
+        configAction.checkPermission();
+    };
+
+    const clearEvents = () => {
+        try {
+            BleManager.disconnect(connectedPeripheralId);
+        } catch (e) {}
+        AppState.removeEventListener('change', handleAppStateChange);
+        handlerDiscoverEmitter.remove();
+        handlerStopScanEmitter.remove();
+        handlerDisconnectEmitter.remove();
+        handlerBleNotificationEmitter.remove();
+        handlerConnectedPerEmitter.remove();
+    };
+
+    const handleAppStateChange = (nextAppState: any) => {
+        if (appState.match(/inactive|background/) && nextAppState === 'active') {
             BleManager.getConnectedPeripherals([]).then((peripheralsArray) => {
                 console.log('Connected peripherals: ' + peripheralsArray.length);
             });
         }
-        this.setState({ appState: nextAppState });
+        setAppState(nextAppState);
     };
 
-    handleConnectedPeripheral = (data: any) => {
-        let peripherals = this.state.peripherals;
+    const handleConnectedPeripheral = (data: any) => {
         let peripheral = peripherals.get(data.peripheral);
         if (peripheral) {
             peripheral.connected = true;
             peripherals.set(peripheral.id, peripheral);
-            this.setState({ peripherals });
         }
     };
 
-    async componentDidMount() {
-        AppState.addEventListener('change', this.handleAppStateChange);
-        await BleManager.start({ showAlert: false });
-        this.handlerDiscoverEmitter = bleManagerEmitter.addListener(
-            'BleManagerDiscoverPeripheral',
-            this.handleDiscoverPeripheral,
-        );
-        this.handlerStopScanEmitter = bleManagerEmitter.addListener('BleManagerStopScan', this.handleStopScan);
-        this.handlerDisconnectEmitter = bleManagerEmitter.addListener(
-            'BleManagerDisconnectPeripheral',
-            this.handleDisconnectedPeripheral,
-        );
-        this.handlerBleNotificationEmitter = bleManagerEmitter.addListener(
-            'BleManagerDidUpdateValueForCharacteristic',
-            this.readBleNotification,
-        );
-        this.handlerConnectedPerEmitter = bleManagerEmitter.addListener(
-            'BleManagerConnectPeripheral',
-            this.handleConnectedPeripheral,
-        );
-        await this.configAction.getMqttInfo();
-        this.configAction.checkPermission();
-    }
+    const flatListItemSeparator = () => {
+        return <View style={styles.divider} />;
+    };
 
-    componentWillUnmount() {
+    const renderItem = (data: any) => {
+        return (
+            <CardData
+                data={data}
+                onPress={() => configAction.connectToPeripheral(data.id)}
+                selectedId={data.connected}
+            />
+        );
+    };
+
+    const handleDisconnectedPeripheral = (data: any) => {
+        let peripheral = peripherals.get(data.peripheral);
+        if (peripheral) {
+            peripheral.connected = false;
+            peripherals.set(peripheral.id, peripheral);
+            connectedPeripheralId = '';
+            setList(Array.from(peripherals.values()));
+            console.log('@Disconnected from ' + data.peripheral);
+        }
+    };
+
+    const startScan = async () => {
         try {
-            BleManager.disconnect(this.connectedPeripheralId);
-        } catch (e) {}
-        AppState.removeEventListener('change', this.handleAppStateChange);
-        this.handlerDiscoverEmitter.remove();
-        this.handlerStopScanEmitter.remove();
-        this.handlerDisconnectEmitter.remove();
-        this.handlerBleNotificationEmitter.remove();
-        this.handlerConnectedPerEmitter.remove();
-    }
-
-    handleDiscoverPeripheral = (peripheral: any) => {
-        let peripherals = this.state.peripherals;
+            peripherals = new Map();
+            setList([]);
+            await BleManager.scan([], 10, false);
+        } catch (e) {
+            console.log('@start scan failed: ', e);
+        }
+    };
+    const handleDiscoverPeripheral = (peripheral: any) => {
         if (peripheral.name) {
             if (__DEV__) {
                 console.log('@Discover Peripheral:', peripheral);
             }
             peripheral.connected = false;
             peripherals.set(peripheral.id, peripheral);
-        }
-        this.setState({ peripherals });
-    };
-
-    handleDisconnectedPeripheral = (data: any) => {
-        let peripherals = this.state.peripherals;
-        let peripheral = peripherals.get(data.peripheral);
-        if (peripheral) {
-            peripheral.connected = false;
-            peripherals.set(peripheral.id, peripheral);
-            this.connectedPeripheralId = '';
-            this.setState({ peripherals });
-        }
-        console.log('@Disconnected from ' + data.peripheral);
-    };
-
-    flatListItemSeparator = () => {
-        return <View style={styles.divider} />;
-    };
-
-    renderItem(data: any) {
-        return (
-            <CardData
-                data={data}
-                onPress={() => this.configAction.connectToPeripheral(data.id)}
-                selectedId={data.connected}
-            />
-        );
-    }
-
-    startScan = async () => {
-        try {
-            this.setState({ peripherals: new Map() });
-            await BleManager.scan([], 10, false);
-        } catch (e) {
-            console.log('@start scan failed: ', e);
+            setList(Array.from(peripherals.values()));
         }
     };
 
-    handleStopScan = () => {
+    const handleStopScan = () => {
         console.log('@handleStopScan');
     };
 
-    readBleNotification = async (res: any) => {
+    const readBleNotification = async (res: any) => {
         try {
             const data = JSON.parse(Parser.parseBytesToString(res.value));
             console.log('@@@RESPONSE BLUETOOTH', data);
             switch (data.type) {
                 case 'init':
                     if (data.status === '1') {
-                        this.props.dispatch(createRequestEndAction());
+                        dispatch(createRequestEndAction());
                         navigate(RouteName.CONFIGURATION_RESULT, null);
                     } else if (data.status === '-1') {
-                        this.props.dispatch(createRequestEndAction());
-                        this.props.dispatch(createRequestErrorMessageAction(data.message));
+                        dispatch(createRequestEndAction());
+                        dispatch(createRequestErrorMessageAction(data.message));
                     }
                     break;
                 default:
@@ -188,52 +171,44 @@ class ConfigurationStep1 extends React.Component {
         }
     };
 
-    handleBack() {
+    return (
+        <View style={styles.container}>
+            <StatusBar translucent backgroundColor="transparent" />
+            <ScrollView style={styles.container} nestedScrollEnabled={true}>
+                <ImageSlider
+                    data={[
+                        'https://source.unsplash.com/wgivdx9dBdQ/1600x900',
+                        'https://source.unsplash.com/wgivdx9dBdQ/1600x900',
+                        'https://source.unsplash.com/wgivdx9dBdQ/1600x900',
+                        'https://source.unsplash.com/wgivdx9dBdQ/1600x900',
+                        'https://source.unsplash.com/wgivdx9dBdQ/1600x900',
+                    ]}
+                    height={imageHeight}
+                />
+                <Text style={styles.title}>Seat#1</Text>
+                <Text style={styles.subTitle}>Autonomous WorkSpace</Text>
+                <Text style={styles.subTitle}>Floor #3</Text>
+                <Text style={styles.subTitle}>Seat #1</Text>
+                <Text style={styles.sectionTitle}>Available assets</Text>
+                <FlatList
+                    nestedScrollEnabled={true}
+                    data={list}
+                    style={[styles.list]}
+                    keyExtractor={(item, index) => `${item}${index}`}
+                    ItemSeparatorComponent={flatListItemSeparator}
+                    renderItem={({ item }) => renderItem(item)}
+                />
+            </ScrollView>
+            <PrimaryButton wrapperContainer={styles.button} title={i18next.t('Scan')} onPress={() => startScan()} />
+            <SafeAreaView style={styles.header}>
+                <BackHeader title={''} lightContent onPress={handleBack} />
+            </SafeAreaView>
+        </View>
+    );
+
+    function handleBack() {
         navigate(RouteName.PLACE_DETAIL, null);
     }
+};
 
-    render() {
-        const imageHeight = 221;
-        const list = Array.from(this.state.peripherals.values());
-        return (
-            <View style={styles.container}>
-                <StatusBar translucent backgroundColor="transparent" />
-                <ScrollView style={styles.container} nestedScrollEnabled={true}>
-                    <ImageSlider
-                        data={[
-                            'https://source.unsplash.com/wgivdx9dBdQ/1600x900',
-                            'https://source.unsplash.com/wgivdx9dBdQ/1600x900',
-                            'https://source.unsplash.com/wgivdx9dBdQ/1600x900',
-                            'https://source.unsplash.com/wgivdx9dBdQ/1600x900',
-                            'https://source.unsplash.com/wgivdx9dBdQ/1600x900',
-                        ]}
-                        height={imageHeight}
-                    />
-                    <Text style={styles.title}>Seat#1</Text>
-                    <Text style={styles.subTitle}>Autonomous WorkSpace</Text>
-                    <Text style={styles.subTitle}>Floor #3</Text>
-                    <Text style={styles.subTitle}>Seat #1</Text>
-                    <Text style={styles.sectionTitle}>Available assets</Text>
-                    <FlatList
-                        nestedScrollEnabled={true}
-                        data={list}
-                        style={[styles.list]}
-                        keyExtractor={(item, index) => `${item}${index}`}
-                        ItemSeparatorComponent={this.flatListItemSeparator}
-                        renderItem={({ item }) => this.renderItem(item)}
-                    />
-                </ScrollView>
-                <PrimaryButton
-                    wrapperContainer={styles.button}
-                    title={i18next.t('Scan')}
-                    onPress={() => this.startScan()}
-                />
-                <SafeAreaView style={styles.header}>
-                    <BackHeader title={''} lightContent onPress={this.handleBack} />
-                </SafeAreaView>
-            </View>
-        );
-    }
-}
-
-export default connect(null, null)(ConfigurationStep1);
+export default ConfigurationStep1;
