34c808ef048278ba826dbcbc61443996c362e9e0
phamhien2102 <phamhien2102@gmail.com>
2021-01-28 14:50:12 +0700

    only owner setup

--- src/helpers/parser.ts
+++ src/helpers/parser.ts
@@ -40,7 +40,7 @@ export class ParserImpl implements IParser {
         result.status = _.get(responseData, 'status');
         result.members = _.get(responseData, 'members');
         result.roleByCurrentUser = _.get(responseData, 'role_by_current_user');
-        result.isAdmin = ROLES.ADMIN == _.get(responseData, 'role_by_current_user');
+        result.isAdmin = ROLES.OWNER == _.get(responseData, 'role_by_current_user');
 
         return result;
     }
