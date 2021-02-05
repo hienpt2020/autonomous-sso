2c1398750a59c781ca80fd608e7c58b9021d1489
phamhien2102 <phamhien2102@gmail.com>
2021-02-05 08:05:16 +0700

    refactor controller

--- src/services/control-device/controller.ts
+++ src/services/control-device/controller.ts
@@ -4,83 +4,76 @@ import { DeviceApi } from '../networking';
 import i18next from 'i18next';
 import store from 'src/redux/store';
 import { createRequestErrorMessageAction } from 'src/redux/request';
+import { Log } from 'src/helpers/logger';
 
 export class Controller {
     private static isNetworking: boolean = true;
-    public static async up(hubId: string, workingLayoutId: string): Promise<any> {
+    public static async up(hubId: string, workingLayoutId: string): Promise<void> {
         try {
             const controller = ControllerFactory.create(this.isNetworking ? API_CONTROLLER : BLE_CONTROLLER);
-            let res = await controller.up(hubId, workingLayoutId);
-            return Promise.resolve(res);
+            await controller.up(hubId, workingLayoutId);
         } catch (e) {
-            return Promise.reject(e);
+            Log.error(e);
         }
     }
-    public static async down(hubId: string, workingLayoutId: string): Promise<any> {
+    public static async down(hubId: string, workingLayoutId: string): Promise<void> {
         try {
             const controller = ControllerFactory.create(this.isNetworking ? API_CONTROLLER : BLE_CONTROLLER);
-            let res = await controller.down(hubId, workingLayoutId);
-            return Promise.resolve(res);
+            await controller.down(hubId, workingLayoutId);
         } catch (e) {
-            return Promise.reject(e);
+            Log.error(e);
         }
     }
-    public static async stop(hubId: string, workingLayoutId: string): Promise<any> {
+    public static async stop(hubId: string, workingLayoutId: string): Promise<void> {
         try {
             const controller = ControllerFactory.create(this.isNetworking ? API_CONTROLLER : BLE_CONTROLLER);
-            let res = await controller.stop(hubId, workingLayoutId);
-            return Promise.resolve(res);
+            await controller.stop(hubId, workingLayoutId);
         } catch (e) {
-            return Promise.reject(e);
+            Log.error(e);
         }
     }
-    public static async getDeviceInfo(hubId: string, workingLayoutId: string): Promise<any> {
+    public static async getDeviceInfo(hubId: string, workingLayoutId: string): Promise<void> {
         try {
             const controller = ControllerFactory.create(this.isNetworking ? API_CONTROLLER : BLE_CONTROLLER);
-            let res = await controller.getDeviceInfo(hubId, workingLayoutId);
-            return Promise.resolve(res);
+            await controller.getDeviceInfo(hubId, workingLayoutId);
         } catch (e) {
-            return Promise.reject(e);
+            Log.error(e);
         }
     }
 
-    public static async stand(hubId: string, workingLayoutId: string): Promise<any> {
+    public static async stand(hubId: string, workingLayoutId: string): Promise<void> {
         try {
             const controller = ControllerFactory.create(this.isNetworking ? API_CONTROLLER : BLE_CONTROLLER);
-            let res = await controller.stand(hubId, workingLayoutId);
-            return Promise.resolve(res);
+            await controller.stand(hubId, workingLayoutId);
         } catch (e) {
-            return Promise.reject(e);
+            Log.error(e);
         }
     }
 
-    public static async sit(hubId: string, workingLayoutId: string): Promise<any> {
+    public static async sit(hubId: string, workingLayoutId: string): Promise<void> {
         try {
             const controller = ControllerFactory.create(this.isNetworking ? API_CONTROLLER : BLE_CONTROLLER);
-            let res = await controller.sit(hubId, workingLayoutId);
-            return Promise.resolve(res);
+            await controller.sit(hubId, workingLayoutId);
         } catch (e) {
-            return Promise.reject(e);
+            Log.error(e);
         }
     }
 
-    public static async gotoHeight(height: number, hubId: string, workingLayoutId: string): Promise<any> {
+    public static async gotoHeight(height: number, hubId: string, workingLayoutId: string): Promise<void> {
         try {
             const controller = ControllerFactory.create(this.isNetworking ? API_CONTROLLER : BLE_CONTROLLER);
-            let res = await controller.gotoHeight(height, hubId, workingLayoutId);
-            return Promise.resolve(res);
+            await controller.gotoHeight(height, hubId, workingLayoutId);
         } catch (e) {
-            return Promise.reject(e);
+            Log.error(e);
         }
     }
 
-    public static async removeDevice(hubId: string): Promise<any> {
+    public static async removeDevice(hubId: string): Promise<void> {
         try {
             let res = await DeviceApi.removeDevice(hubId);
-            return Promise.resolve(res);
         } catch (e) {
             store.dispatch(createRequestErrorMessageAction(i18next.t('common.error_message')));
-            return Promise.reject(e);
+            Log.error(e);
         }
     }
 }
