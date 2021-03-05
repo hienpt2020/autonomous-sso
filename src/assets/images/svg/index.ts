ec56e6858881d9700353d52e090dbd4e44391a9e
phamhien2102 <phamhien2102@gmail.com>
2021-03-05 09:26:41 +0700

    update codebase foundation

--- src/assets/images/svg/index.ts
+++
@@ -1,5 +0,0 @@
-export { IcDown } from './IcDown';
-export { IcUp } from './IcUp';
-export { IcSignal } from './IcSignal';
-export { IcStand } from './IcStand';
-export { IcSit } from './IcSit';
--- src/screens/intro/types.ts
+++
@@ -1,9 +0,0 @@
-import { PropsBase } from 'src/types/propsBase';
-import { RouteName } from 'src/routers/routeName';
-
-export enum VerifyState {
-    VERIFYING,
-    VALID,
-    INVALID,
-}
-export interface Props extends PropsBase<RouteName.INTRO> {}
